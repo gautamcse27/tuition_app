ad94884f728f8eea843725fbfad2764016ef07aa
gautamkumar <khushi27gkcse@gmail.com>
2025-09-28 14:56:10 +0530

    Add

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-uri = os.getenv('DATABASE_URL', 'postgresql://tuition25:Dtuition@localhost:5432/tuitiondb')
+uri = os.getenv('postgres://udckrcs745a6dk:p695bd087556dd6d38ff2bd6e7bd1eb087732a6049f74f31d22e9033d1458a9af@c7itisjfjj8ril.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d6ciuah5gg7vj3', 'postgresql://tuition25:Dtuition@localhost:5432/tuitiondb')
 
 # Fix for Heroku deprecated 'postgres://' prefix
 if uri.startswith('postgres://'):
