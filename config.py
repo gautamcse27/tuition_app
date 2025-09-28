0da48557061c71c9cd9edf26bd685ede5c5167f0
gautamkumar <khushi27gkcse@gmail.com>
2025-09-28 14:25:47 +0530

    Fix database config for Heroku

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','postgresql://tuition25:Dtuition@localhost:5432/tuitiondb')
+
+uri = os.getenv('DATABASE_URL', 'postgresql://tuition25:Dtuition@localhost:5432/tuitiondb')
+
+# Fix for Heroku deprecated 'postgres://' prefix
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
+
+SQLALCHEMY_DATABASE_URI = uri
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'ff8740764e0833b0b849259bb981b70d9c5da1c8eef39d82'
