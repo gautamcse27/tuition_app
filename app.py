65f16197b3cb60be17d84f954ccd4014ccf6bf7f
gautamkumar <khushi27gkcse@gmail.com>
2025-10-28 11:39:21 +0530

    Your commit message

--- app.py
+++ app.py
@@ -181,5 +181,5 @@ def payment(req_id):
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
